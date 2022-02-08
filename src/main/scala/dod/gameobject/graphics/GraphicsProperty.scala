a750397d3747c0ddfcac573d2ca617e974d3c96b
majki <michofilip@gmail.com>
2022-02-08 07:30:53 +0100

    fix

--- src/main/scala/dod/gameobject/graphics/GraphicsProperty.scala
+++ src/main/scala/dod/gameobject/graphics/GraphicsProperty.scala
@@ -3,7 +3,7 @@ package dod.gameobject.graphics
 import dod.gameobject.position.Direction
 import dod.gameobject.state.State
 
-class GraphicsProperty(val level: Int, private[graphics] val animationSelector: AnimationSelector) {
+class GraphicsProperty(private[graphics] val level: Int, private[graphics] val animationSelector: AnimationSelector) {
     def animation(state: Option[State], direction: Option[Direction]): Animation =
         animationSelector.selectAnimation(state, direction)
 }
