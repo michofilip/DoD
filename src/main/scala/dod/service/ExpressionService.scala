1fdf07af8862bcb55bfef86616c98b68b7371a6f
majki <michofilip@gmail.com>
2022-04-11 21:44:18 +0200

    expr refactor 1

--- src/main/scala/dod/service/ExpressionService.scala
+++ src/main/scala/dod/service/ExpressionService.scala
@@ -2,6 +2,8 @@ package dod.service
 
 import dod.game.expression.{BooleanExpr, CoordinatesExpr, DecimalExpr, DirectionExpr, Expr, GameObjectExpr, IntegerExpr, ShiftExpr, StringExpr, TimestampExpr}
 import dod.game.gameobject.GameObjectRepository
+import dod.game.model.Timestamps.Timestamp
+import dod.game.model.{Coordinates, Direction, Shift}
 
 import scala.math
 import scala.math.Ordered
@@ -9,6 +11,25 @@ import scala.math.Ordered
 class ExpressionService {
 
     def resolve[T](expr: Expr[T])(using gor: GameObjectRepository): Option[T] = expr match {
+        case expr: BooleanExpr => resolveBooleanExpr(expr)
+        case expr: IntegerExpr => resolveIntegerExpr(expr)
+        case expr: DecimalExpr => resolveDecimalExpr(expr)
+        case expr: StringExpr => resolveStringExpr(expr)
+        case expr: TimestampExpr => resolveTimestampExpr(expr)
+        case expr: CoordinatesExpr => resolveCoordinatesExpr(expr)
+        case expr: ShiftExpr => resolveShiftExpr(expr)
+        case expr: DirectionExpr => resolveDirectionExpr(expr)
+
+        //        case GameObjectExpr.GetName(id) => gor.findById(id).map(_.name)
+        //        case GameObjectExpr.GetCreationTimestamp(id) => gor.findById(id).map(_.creationTimestamp)
+        //        case GameObjectExpr.GetCoordinates(id) => gor.findById(id).flatMap(_.position.coordinates)
+        //        case GameObjectExpr.GetDirection(id) => gor.findById(id).flatMap(_.position.direction)
+        //        case GameObjectExpr.GetPositionTimestamp(id) => gor.findById(id).flatMap(_.position.positionTimestamp)
+
+        case _ => None
+    }
+
+    private inline def resolveBooleanExpr(expr: BooleanExpr)(using gor: GameObjectRepository): Option[Boolean] = expr match {
         case BooleanExpr.Constant(value) => Some(value)
         case BooleanExpr.Not(expr) => res1(expr) { x => Some(!x) }
         case BooleanExpr.And(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(x && y) }
@@ -19,7 +40,9 @@ class ExpressionService {
         case BooleanExpr.LessEquals(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(expr1.lteq(x, y)) }
         case BooleanExpr.Greater(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(expr1.gt(x, y)) }
         case BooleanExpr.GreaterEquals(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(expr1.gteq(x, y)) }
+    }
 
+    private inline def resolveIntegerExpr(expr: IntegerExpr)(using gor: GameObjectRepository): Option[Int] = expr match {
         case IntegerExpr.Constant(value) => Some(value)
         case IntegerExpr.Negation(expr) => res1(expr) { x => Some(-x) }
         case IntegerExpr.Addition(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(x + y) }
@@ -28,7 +51,9 @@ class ExpressionService {
         case IntegerExpr.Division(expr1, expr2) => res2(expr1, expr2) { (x, y) => if y != 0 then Some(x / y) else None }
         case IntegerExpr.Reminder(expr1, expr2) => res2(expr1, expr2) { (x, y) => if y != 0 then Some(x % y) else None }
         case IntegerExpr.DecimalToInteger(expr) => res1(expr) { x => Some(x.toInt) }
+    }
 
+    private inline def resolveDecimalExpr(expr: DecimalExpr)(using gor: GameObjectRepository): Option[Double] = expr match {
         case DecimalExpr.Constant(value) => Some(value)
         case DecimalExpr.Negation(expr) => res1(expr) { x => Some(-x) }
         case DecimalExpr.Addition(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(x + y) }
@@ -36,28 +61,30 @@ class ExpressionService {
         case DecimalExpr.Multiplication(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(x * y) }
         case DecimalExpr.Division(expr1, expr2) => res2(expr1, expr2) { (x, y) => if y != 0 then Some(x / y) else None }
         case DecimalExpr.IntegerToDecimal(expr) => res1(expr) { x => Some(x.toDouble) }
+    }
 
+    private inline def resolveStringExpr(expr: StringExpr)(using gor: GameObjectRepository): Option[String] = expr match {
         case StringExpr.Constant(value) => Some(value)
         case StringExpr.Concatenate(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(x + y) }
         case StringExpr.IntegerToString(expr) => res1(expr) { x => Some(x.toString) }
         case StringExpr.DecimalToString(expr) => res1(expr) { x => Some(x.toString) }
+    }
 
+    private inline def resolveTimestampExpr(expr: TimestampExpr)(using gor: GameObjectRepository): Option[Timestamp] = expr match {
         case TimestampExpr.Constant(value) => Some(value)
+    }
 
+    private inline def resolveCoordinatesExpr(expr: CoordinatesExpr)(using gor: GameObjectRepository): Option[Coordinates] = expr match {
         case CoordinatesExpr.Constant(value) => Some(value)
         case CoordinatesExpr.MoveBy(expr1, expr2) => res2(expr1, expr2) { (x, y) => Some(x.moveBy(y)) }
+    }
 
+    private inline def resolveShiftExpr(expr: ShiftExpr)(using gor: GameObjectRepository): Option[Shift] = expr match {
         case ShiftExpr.Constant(value) => Some(value)
+    }
 
+    private inline def resolveDirectionExpr(expr: DirectionExpr)(using gor: GameObjectRepository): Option[Direction] = expr match {
         case DirectionExpr.Constant(value) => Some(value)
-
-        case GameObjectExpr.GetName(id) => gor.findById(id).map(_.name)
-        case GameObjectExpr.GetCreationTimestamp(id) => gor.findById(id).map(_.creationTimestamp)
-        case GameObjectExpr.GetCoordinates(id) => gor.findById(id).flatMap(_.position.coordinates)
-        case GameObjectExpr.GetDirection(id) => gor.findById(id).flatMap(_.position.direction)
-        case GameObjectExpr.GetPositionTimestamp(id) => gor.findById(id).flatMap(_.position.positionTimestamp)
-
-        case _ => None
     }
 
 
