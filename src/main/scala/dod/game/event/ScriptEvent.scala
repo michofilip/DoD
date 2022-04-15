79c34e1a1c3855b1e5b7a6740929260c1ff21608
majki <michofilip@gmail.com>
2022-04-15 22:48:56 +0200

    ScriptEvent fix

--- src/main/scala/dod/game/event/ScriptEvent.scala
+++ src/main/scala/dod/game/event/ScriptEvent.scala
@@ -5,6 +5,5 @@ import dod.game.model.Script
 import java.util.UUID
 
 enum ScriptEvent extends Event {
-    case RunScript(gameObjectId: UUID, scriptName: String)
-    case ExecuteScriptLine(script: Script, lineNo: Int)
+    case RunScript(gameObjectId: UUID, scriptName: String, lineNo: Int = 0)
 }
--- src/main/scala/dod/service/event/ScriptEventService.scala
+++ src/main/scala/dod/service/event/ScriptEventService.scala
@@ -12,34 +12,31 @@ import java.util.UUID
 private[event] final class ScriptEventService(expressionService: ExpressionService) {
 
     def processScriptEvent(gameObjectRepository: GameObjectRepository, scriptEvent: ScriptEvent): EventResponse = scriptEvent match {
-        case ScriptEvent.RunScript(gameObjectId, scriptName) => ???
-
-        case ScriptEvent.ExecuteScriptLine(script, lineNo) =>
-            handleExecuteScriptLine(gameObjectRepository, script, lineNo)
+        case ScriptEvent.RunScript(gameObjectId, scriptName, lineNo) =>
+            handleRunScript(gameObjectRepository, gameObjectId, scriptName, lineNo)
     }
 
-    private def handleExecuteScriptLine(gameObjectRepository: GameObjectRepository, script: Script, lineNo: Int): EventResponse = {
+    private def handleRunScript(gameObjectRepository: GameObjectRepository, gameObjectId: UUID, scriptName: String, lineNo: Int): EventResponse = {
         given GameObjectRepository = gameObjectRepository
 
-        val responseEvents = script.getNext(lineNo) match {
-            case (_, EXIT(_)) =>
-                Seq.empty
-
-            case (nextLineNo, EXECUTE(events)) =>
-                ScriptEvent.ExecuteScriptLine(script, nextLineNo + 1) +: events
-
-            case (nextLineNo, TEST(condition)) =>
-                expressionService.resolve(condition) match {
-                    case Some(true) => Seq(ScriptEvent.ExecuteScriptLine(script, nextLineNo + 2))
-                    case Some(false) => Seq(ScriptEvent.ExecuteScriptLine(script, nextLineNo + 1))
+        for
+            script <- gameObjectRepository.findScript(gameObjectId, scriptName)
+        yield {
+            val responseEvents = script.getNext(lineNo) match {
+                case (_, EXIT(_)) => Seq.empty
+                case (nextLineNo, EXECUTE(events)) => ScriptEvent.RunScript(gameObjectId, scriptName, nextLineNo + 1) +: events
+                case (nextLineNo, TEST(condition)) => expressionService.resolve(condition) match {
+                    case Some(true) => Seq(ScriptEvent.RunScript(gameObjectId, scriptName, nextLineNo + 2))
+                    case Some(false) => Seq(ScriptEvent.RunScript(gameObjectId, scriptName, nextLineNo + 1))
                     case None => Seq.empty
                 }
+                case _ => Seq.empty
+            }
 
-            case _ =>
-                Seq.empty
+            (gameObjectRepository, responseEvents)
         }
-
-        (gameObjectRepository, responseEvents)
+    }.getOrElse {
+        (gameObjectRepository, Seq.empty)
     }
 
 }
