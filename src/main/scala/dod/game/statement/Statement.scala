8555735e7033f3f3b352ed3868485425557f602c
majki <michofilip@gmail.com>
2022-04-18 20:18:53 +0200

    statements update

--- src/main/scala/dod/game/statement/Statement.scala
+++ src/main/scala/dod/game/statement/Statement.scala
@@ -12,8 +12,8 @@ object Statement {
     final case class Block(statements: Seq[Statement]) extends Statement
 
     final case class When(condition: BooleanExpr) {
-        def therefore(statements: Statement*): MultiWhenTherefore =
-            MultiWhenTherefore(Vector(WhenTherefore(condition, Block(statements))))
+        def therefore(statement: Statement, statements: Statement*): MultiWhenTherefore =
+            MultiWhenTherefore(Vector(WhenTherefore(condition, Block(statement +: statements))))
     }
 
     final case class WhenTherefore(condition: BooleanExpr, thereforeStatement: Statement)
@@ -22,20 +22,20 @@ object Statement {
         def otherwiseWhen(condition: BooleanExpr): MultiWhenThereforeWhen =
             MultiWhenThereforeWhen(whenThereforeSeq, condition)
 
-        def otherwise(statements: Statement*): MultiWhenThereforeOtherwise =
-            MultiWhenThereforeOtherwise(whenThereforeSeq, Block(statements))
+        def otherwise(statement: Statement, statements: Statement*): MultiWhenThereforeOtherwise =
+            MultiWhenThereforeOtherwise(whenThereforeSeq, Block(statement +: statements))
     }
 
     final case class MultiWhenThereforeWhen(whenThereforeSeq: IndexedSeq[WhenTherefore], condition: BooleanExpr) {
-        def therefore(statements: Statement*): MultiWhenTherefore =
-            MultiWhenTherefore(whenThereforeSeq :+ WhenTherefore(condition, Block(statements)))
+        def therefore(statement: Statement, statements: Statement*): MultiWhenTherefore =
+            MultiWhenTherefore(whenThereforeSeq :+ WhenTherefore(condition, Block(statement +: statements)))
     }
 
     final case class MultiWhenThereforeOtherwise(whenThereforeSeq: IndexedSeq[WhenTherefore], otherwiseStatement: Statement) extends Statement
 
     final case class Loop(condition: BooleanExpr) {
-        def body(statements: Statement*): LoopBody =
-            LoopBody(condition, Block(statements))
+        def body(statement: Statement, statements: Statement*): LoopBody =
+            LoopBody(condition, Block(statement +: statements))
     }
 
     final case class LoopBody(condition: BooleanExpr, body: Statement) extends Statement
@@ -44,13 +44,13 @@ object Statement {
         def when(condition: BooleanExpr): VariantWhen[T] =
             VariantWhen(expressions, Some(condition))
 
-        def therefore(statements: Statement*): VariantWhenTherefore[T] =
-            VariantWhenTherefore(expressions, None, Block(statements))
+        def therefore(statement: Statement, statements: Statement*): VariantWhenTherefore[T] =
+            VariantWhenTherefore(expressions, None, Block(statement +: statements))
     }
 
     final case class VariantWhen[T](expressions: Seq[Expr[T]], conditionOpt: Option[BooleanExpr]) {
-        def therefore(statements: Statement*): VariantWhenTherefore[T] =
-            VariantWhenTherefore(expressions, conditionOpt, Block(statements))
+        def therefore(statement: Statement, statements: Statement*): VariantWhenTherefore[T] =
+            VariantWhenTherefore(expressions, conditionOpt, Block(statement +: statements))
     }
 
     final case class VariantWhenTherefore[T](expressions: Seq[Expr[T]], conditionOpt: Option[BooleanExpr], therefore: Statement)
@@ -61,8 +61,8 @@ object Statement {
     }
 
     final case class ChooseVariants[T](expression: Expr[T], variants: Seq[VariantWhenTherefore[T]]) extends Statement {
-        def otherwise(statements: Statement*): ChooseVariantsOtherwise[T] =
-            ChooseVariantsOtherwise(expression, variants, Block(statements))
+        def otherwise(statement: Statement, statements: Statement*): ChooseVariantsOtherwise[T] =
+            ChooseVariantsOtherwise(expression, variants, Block(statement +: statements))
     }
 
     final case class ChooseVariantsOtherwise[T](expression: Expr[T], variants: Seq[VariantWhenTherefore[T]], otherwise: Statement) extends Statement
