2390109274e064650c21df2110bb50f5fd1f42e4
majki <michofilip@gmail.com>
2022-04-28 18:56:55 +0200

    expressions-in-events - PositionEvent updated 2

--- src/main/scala/dod/service/event/PositionEventService.scala
+++ src/main/scala/dod/service/event/PositionEventService.scala
@@ -1,6 +1,7 @@
 package dod.service.event
 
 import dod.game.event.{Event, PositionEvent}
+import dod.game.expression.Expr
 import dod.game.gameobject.position.PositionTransformer
 import dod.game.gameobject.{GameObject, GameObjectRepository}
 import dod.game.model.Timestamps.Timestamp
@@ -12,64 +13,84 @@ import scala.util.chaining.scalaUtilChainingOps
 private[event] final class PositionEventService {
 
     def processPositionEvent(positionEvent: PositionEvent)(using gameObjectRepository: GameObjectRepository): EventResponse = positionEvent match {
-        case PositionEvent.MoveTo(gameObjectId, coordinates) =>
-            coordinates.get.fold(defaultResponse) { coordinates =>
-                handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.moveTo(coordinates))
-            }
+        case PositionEvent.MoveTo(gameObjectId, coordinates) => (gameObjectId, coordinates) ~> {
+            (gameObjectId, coordinates) => handlePositionUpdate(gameObjectId, PositionTransformer.moveTo(coordinates))
+        }
 
-        case PositionEvent.MoveBy(gameObjectId, shift) =>
-            shift.get.fold(defaultResponse) { shift =>
-                handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.moveBy(shift))
-            }
+        case PositionEvent.MoveBy(gameObjectId, shift) => (gameObjectId, shift) ~> {
+            (gameObjectId, shift) => handlePositionUpdate(gameObjectId, PositionTransformer.moveBy(shift))
+        }
 
-        case PositionEvent.TurnTo(gameObjectId, direction) =>
-            direction.get.fold(defaultResponse) { direction =>
-                handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.turnTo(direction))
-            }
+        case PositionEvent.TurnTo(gameObjectId, direction) => (gameObjectId, direction) ~> {
+            (gameObjectId, direction) => handlePositionUpdate(gameObjectId, PositionTransformer.turnTo(direction))
+        }
 
-        case PositionEvent.TurnClockwise(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.turnClockwise)
+        case PositionEvent.TurnClockwise(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.turnClockwise)
+        }
 
-        case PositionEvent.TurnCounterClockwise(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.turnCounterClockwise)
+        case PositionEvent.TurnCounterClockwise(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.turnCounterClockwise)
+        }
 
-        case PositionEvent.TurnBack(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.turnBack)
+        case PositionEvent.TurnBack(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.turnBack)
+        }
 
-        case PositionEvent.Step(gameObjectId, direction) =>
-            direction.get.fold(defaultResponse) { direction =>
-                handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.step(direction))
-            }
+        case PositionEvent.Step(gameObjectId, direction) => (gameObjectId, direction) ~> {
+            (gameObjectId, direction) => handlePositionUpdate(gameObjectId, PositionTransformer.step(direction))
+        }
 
-        case PositionEvent.StepForward(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepForward)
+        case PositionEvent.StepForward(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepForward)
+        }
 
-        case PositionEvent.StepRight(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepRight)
+        case PositionEvent.StepRight(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepRight)
+        }
 
-        case PositionEvent.StepLeft(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepLeft)
+        case PositionEvent.StepLeft(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepLeft)
+        }
 
-        case PositionEvent.StepBack(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepBack)
+        case PositionEvent.StepBack(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepBack)
+        }
 
-        case PositionEvent.StepAndFace(gameObjectId, direction) =>
-            direction.get.fold(defaultResponse) { direction =>
-                handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepAndFace(direction))
-            }
+        case PositionEvent.StepAndFace(gameObjectId, direction) => (gameObjectId, direction) ~> {
+            (gameObjectId, direction) => handlePositionUpdate(gameObjectId, PositionTransformer.stepAndFace(direction))
+        }
 
-        case PositionEvent.StepRightAndFace(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepRightAndFace)
+        case PositionEvent.StepRightAndFace(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepRightAndFace)
+        }
 
-        case PositionEvent.StepLeftAndFace(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepLeftAndFace)
+        case PositionEvent.StepLeftAndFace(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepLeftAndFace)
+        }
 
-        case PositionEvent.StepBackAndFace(gameObjectId) =>
-            handlePositionUpdate(gameObjectRepository, gameObjectId, PositionTransformer.stepBackAndFace)
+        case PositionEvent.StepBackAndFace(gameObjectId) => gameObjectId ~> {
+            gameObjectId => handlePositionUpdate(gameObjectId, PositionTransformer.stepBackAndFace)
+        }
     }
 
+    extension[T] (using GameObjectRepository)(expr: Expr[T]) {
+        private def ~>(f: T => EventResponse): EventResponse = handle1(expr)(f)
+    }
 
-    private def handlePositionUpdate(gameObjectRepository: GameObjectRepository, gameObjectId: String, positionTransformer: PositionTransformer): EventResponse = {
+    extension[T1, T2] (using GameObjectRepository)(pair: (Expr[T1], Expr[T2])) {
+        private def ~>(f: (T1, T2) => EventResponse): EventResponse = handle2(pair._1, pair._2)(f)
+    }
+
+    inline private def handle1[T](e: Expr[T])(f: T => EventResponse)(using GameObjectRepository): EventResponse = {
+        for (e <- e.get) yield f(e)
+    }.getOrElse(defaultResponse)
+
+    inline private def handle2[T1, T2](e1: Expr[T1], e2: Expr[T2])(f: (T1, T2) => EventResponse)(using GameObjectRepository): EventResponse = {
+        for (e1 <- e1.get; e2 <- e2.get) yield f(e1, e2)
+    }.getOrElse(defaultResponse)
+
+    inline private def handlePositionUpdate(gameObjectId: String, positionTransformer: PositionTransformer)(using gameObjectRepository: GameObjectRepository): EventResponse = {
         gameObjectRepository.findById(gameObjectId).map { gameObject =>
             val timestamp = gameObjectRepository.findTimer("global_timers", "timer_1").fold(Timestamp.zero)(_.timestamp)
 
@@ -77,10 +98,9 @@ private[event] final class PositionEventService {
         }.collect { case (gameObjectRepository, gameObject) if canUpdatePosition(gameObjectRepository, gameObject) =>
             (gameObjectRepository + gameObject, Seq.empty)
         }.getOrElse {
-            (gameObjectRepository, Seq.empty)
+            defaultResponse
         }
     }
-
 
     inline private def canUpdatePosition(gameObjectRepository: GameObjectRepository, gameObjectUpdated: GameObject): Boolean =
         !gameObjectUpdated.position.coordinates.exists(gameObjectRepository.existSolidAtCoordinates)
