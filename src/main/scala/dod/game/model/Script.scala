414cbc1bef8fb456d27347c9c1ef8504568a8532
majki <michofilip@gmail.com>
2022-05-02 18:53:56 +0200

    script-service - script cleanup

--- src/main/scala/dod/game/model/Script.scala
+++ src/main/scala/dod/game/model/Script.scala
@@ -6,31 +6,9 @@ import scala.annotation.tailrec
 
 final case class Script(instructions: IndexedSeq[Instruction]) {
 
-    private val scriptLength = instructions.length
-
-    //    private 
-    val labelMap: Map[Int, Int] =
+    val labels: Map[Int, Int] =
         instructions.zipWithIndex.collect {
             case (LABEL(labelId), lineNo) => labelId -> lineNo
         }.toMap
-
-
-    @tailrec
-    def nextExecutableLine(lineNo: Int): (Int, Instruction) =
-        getInstruction(lineNo) match {
-            case LABEL(_) => nextExecutableLine(lineNo + 1)
-            case GOTO(labelId) => labelMap.get(labelId) match {
-                case Some(lineNo) => nextExecutableLine(lineNo + 1)
-                case None => (lineNo, EXIT(2))
-            }
-            case instruction => (lineNo, instruction)
-        }
-
-    //    private inline
-    def getInstruction(lineNo: Int): Instruction =
-        if (0 <= lineNo && lineNo < scriptLength)
-            instructions(lineNo)
-        else
-            EXIT(1)
 
 }
--- src/main/scala/dod/service/ScriptService.scala
+++ src/main/scala/dod/service/ScriptService.scala
@@ -10,22 +10,25 @@ object ScriptService {
 
     @tailrec
     def nextExecutableLine(script: Script, lineNo: Int)(using GameObjectRepository): (Int, Instruction) = {
-        script.getInstruction(lineNo) match {
+        inline def getInstruction(lineNo: Int): Instruction =
+            if 0 <= lineNo && lineNo < script.instructions.length then
+                script.instructions(lineNo)
+            else
+                EXIT(1)
+
+        getInstruction(lineNo) match
             case LABEL(_) => nextExecutableLine(script, lineNo + 1)
 
-            case GOTO(labelId) => script.labelMap.get(labelId) match {
+            case GOTO(labelId) => script.labels.get(labelId) match
                 case Some(lineNo) => nextExecutableLine(script, lineNo + 1)
                 case None => (lineNo, EXIT(2))
-            }
 
-            case TEST(condition) => condition.get match {
+            case TEST(condition) => condition.get match
                 case Some(true) => nextExecutableLine(script, lineNo + 2)
                 case Some(false) => nextExecutableLine(script, lineNo + 1)
                 case None => (lineNo, EXIT(3))
-            }
 
             case instruction => (lineNo, instruction)
-        }
     }
 
 }
