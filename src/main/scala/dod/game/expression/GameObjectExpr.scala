db3dbc5244307d863006150eebbc424ec7520af8
Michał Filipowski <michofilip@gmail.com>
2022-05-02 16:17:24 +0200

    Gameobject expressions 2 (#27)
    * expression-property-2 - GameObjectExpr with expr
    
    * expression-property-2 - ScriptEventService partial fix

--- src/main/scala/dod/game/expression/GameObjectExpr.scala
+++ src/main/scala/dod/game/expression/GameObjectExpr.scala
@@ -1,10 +1,11 @@
 package dod.game.expression
 
 
-import dod.game.gameobject.GameObjectRepository
-import dod.game.model.Timestamps.Timestamp
-import dod.game.model.{Coordinates, Direction, State}
 import dod.game.expression.Expr.ExprContext
+import dod.game.gameobject.GameObjectRepository
+import dod.game.model.Durations.Duration
+import dod.game.model.Timestamps.Timestamp
+import dod.game.model.{Coordinates, Direction, Shift, State}
 
 object GameObjectExpr {
 
@@ -42,5 +43,46 @@ object GameObjectExpr {
     final case class GetSolid(id: String) extends BooleanExpr :
         override def get(using ExprContext): Option[Boolean] =
             summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.physics).map(_.solid)
+
+
+    final case class GetBooleanExpr(id: String, exprName: String) extends BooleanExpr :
+        override def get(using ExprContext): Option[Boolean] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.booleanExpr(exprName)).flatMap(_.get)
+
+    final case class GetIntegerExpr(id: String, exprName: String) extends IntegerExpr :
+        override def get(using ExprContext): Option[Int] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.integerExpr(exprName)).flatMap(_.get)
+
+    final case class GetDecimalExpr(id: String, exprName: String) extends DecimalExpr :
+        override def get(using ExprContext): Option[Double] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.decimalExpr(exprName)).flatMap(_.get)
+
+    final case class GetStringExpr(id: String, exprName: String) extends StringExpr :
+        override def get(using ExprContext): Option[String] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.stringExpr(exprName)).flatMap(_.get)
+
+    final case class GetTimestampExpr(id: String, exprName: String) extends TimestampExpr :
+        override def get(using ExprContext): Option[Timestamp] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.timestampExpr(exprName)).flatMap(_.get)
+
+    final case class GetDurationExpr(id: String, exprName: String) extends DurationExpr :
+        override def get(using ExprContext): Option[Duration] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.durationExpr(exprName)).flatMap(_.get)
+
+    final case class GetCoordinatesExpr(id: String, exprName: String) extends CoordinatesExpr :
+        override def get(using ExprContext): Option[Coordinates] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.coordinatesExpr(exprName)).flatMap(_.get)
+
+    final case class GetShiftExpr(id: String, exprName: String) extends ShiftExpr :
+        override def get(using ExprContext): Option[Shift] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.shiftExpr(exprName)).flatMap(_.get)
+
+    final case class GetDirectionExpr(id: String, exprName: String) extends DirectionExpr :
+        override def get(using ExprContext): Option[Direction] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.directionExpr(exprName)).flatMap(_.get)
+
+    final case class GetStateExpr(id: String, exprName: String) extends StateExpr :
+        override def get(using ExprContext): Option[State] =
+            summon[ExprContext].asInstanceOf[GameObjectRepository].findById(id).flatMap(_.expressions.stateExpr(exprName)).flatMap(_.get)
 
 }
