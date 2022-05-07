33c2c23816fa1110c7c14bd60c1762513d3c97fc
majki <michofilip@gmail.com>
2022-05-07 21:46:49 +0200

    actors-refactor - GameActor refactor

--- src/main/scala/dod/actor/GameActor.scala
+++ src/main/scala/dod/actor/GameActor.scala
@@ -3,7 +3,7 @@ package dod.actor
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import dod.actor
-import dod.actor.GameActor.Command
+import dod.actor.GameActor.{Command, Exit, GameStageCommand}
 import dod.service.KeyEventService
 import dod.service.event.EventService
 import dod.ui.Screen
@@ -11,13 +11,18 @@ import scalafx.scene.input.{KeyCode, KeyEvent}
 
 final class GameActor private(gameStageActor: ActorRef[GameStageActor.Command]) {
     private def behavior(): Behavior[Command] = Behaviors.receiveMessage {
-        case GameActor.Exit =>
-            Behaviors.stopped
-
-        case GameActor.GameStageCommand(command) =>
-            gameStageActor ! command
-            Behaviors.same
+        case Exit => handleExit
+        case GameStageCommand(command) => handleGameStageCommand(command)
     }
+
+    private inline def handleExit: Behavior[Command] =
+        Behaviors.stopped
+
+    private inline def handleGameStageCommand(command: GameStageActor.Command): Behavior[Command] = {
+        gameStageActor ! command
+        Behaviors.same
+    }
+
 }
 
 object GameActor {
@@ -27,6 +32,7 @@ object GameActor {
     case object Exit extends Command
 
     final case class GameStageCommand(command: GameStageActor.Command) extends Command
+
 
     def apply(eventService: EventService, screen: Screen, keyEventService: KeyEventService): Behavior[Command] = Behaviors.setup { context =>
 
